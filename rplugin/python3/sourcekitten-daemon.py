afb403df1cd7896eb4a3f1182d7fa8d686d584cf
Tyler Casselman <tylerc230@gmail.com>
2017-07-15 17:22:55 -0700

    Add skd process termination method

--- rplugin/python3/sourcekitten-daemon.py
+++ rplugin/python3/sourcekitten-daemon.py
@@ -4,11 +4,24 @@ import neovim
 class SourceKittenDaemon(object):
     def __init__(self, vim):
         self.vim = vim
+        self.job_id = None
 
     @neovim.function('LaunchSourceKitten')
     def launch(self, args):
         project_name = args[0]
         port = args[1]
-        job = f"sourcekittendaemon start --project {project_name} --port {port}"
-        self.vim.funcs.jobstart(job)
+        self._launch(project_name, port)
+
+    @neovim.function('TerminateSourceKitten')
+    def terminate(self):
+        if self.job_id != None:
+            self.vim.funcs.jobstop(self.job_id)
+            self.job_id = None
+
+    def _launch(self, project_name, port):
+        if self.job_id == None:
+            job = f"sourcekittendaemon start --project {project_name} --port {port}"
+            self.job_id = self.vim.funcs.jobstart(job)
+
+
 
