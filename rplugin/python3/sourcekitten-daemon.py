bc7000038d60a40d401381195a3990b4719b3a6d
Tyler Casselman <tylerc230@gmail.com>
2017-07-17 10:11:23 -0700

    Check if xcode project exists if not, throw exception

--- rplugin/python3/sourcekitten-daemon.py
+++ rplugin/python3/sourcekitten-daemon.py
@@ -1,4 +1,5 @@
 import neovim
+from pathlib import Path
 
 @neovim.plugin
 class SourceKittenDaemon(object):
@@ -10,7 +11,13 @@ class SourceKittenDaemon(object):
     def launch(self, args):
         project_name = args[0]
         port = args[1]
-        self._launch(project_name, port)
+        try:
+            self._launch(project_name, port)
+        except Exception as error:
+            msg = error.args[0]
+            print(msg)
+            self._echom(msg)
+
 
     @neovim.function('TerminateSourceKitten')
     def terminate(self):
@@ -19,9 +26,26 @@ class SourceKittenDaemon(object):
             self.job_id = None
 
     def _launch(self, project_name, port):
+        if not self._check_project_exists(project_name):
+            raise FileNotFoundError(f"Couldn't find project {project_name}")
         if self.job_id == None:
             job = f"sourcekittendaemon start --project {project_name} --port {port}"
             self.job_id = self.vim.funcs.jobstart(job)
+            if self.job_id == None:
+                raise SystemError(f"Failed to start job: {job}")
+        else:
+            raise SystemError(f"Daemon already running at job id {self.job_id}")
+
+    def _check_project_exists(self, project_name):
+        project_path = Path(project_name)
+        if project_path.exists():
+            return True
+        else:
+            return False
+
+    def _echom(self, msg):
+        self.vim.command(f"echom \"{msg}\"")
+
 
 
 
