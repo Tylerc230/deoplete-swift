329d658140333c3064982873da09119365ec4571
Tyler Casselman <tylerc230@gmail.com>
2017-07-17 10:49:39 -0700

    Add vex.yaml project file
    This contains info about which xcode projects to load.

--- rplugin/python3/sourcekitten-daemon.py
+++ rplugin/python3/sourcekitten-daemon.py
@@ -1,4 +1,5 @@
 import neovim
+import yaml
 from pathlib import Path
 
 @neovim.plugin
@@ -7,15 +8,23 @@ class SourceKittenDaemon(object):
         self.vim = vim
         self.job_id = None
 
-    @neovim.function('LaunchSourceKitten')
-    def launch(self, args):
+    @neovim.function('LaunchSourceKittenWithXCodeProj')
+    def launch_with_xcodeproj(self, args):
         project_name = args[0]
         port = args[1]
         try:
             self._launch(project_name, port)
         except Exception as error:
             msg = error.args[0]
-            print(msg)
+            self._echom(msg)
+
+    @neovim.function('LaunchSourceKittenFromVexProject')
+    def launch_from_vex_project(self, args):
+        try:
+            vexproj = self._load_vex_project()
+            self._launch(vexproj['xcodeproj'], 8081)
+        except Exception as error:
+            msg = error.args[0]
             self._echom(msg)
 
 
@@ -26,8 +35,7 @@ class SourceKittenDaemon(object):
             self.job_id = None
 
     def _launch(self, project_name, port):
-        if not self._check_project_exists(project_name):
-            raise FileNotFoundError(f"Couldn't find project {project_name}")
+        self._check_file_exists(project_name)
         if self.job_id == None:
             job = f"sourcekittendaemon start --project {project_name} --port {port}"
             self.job_id = self.vim.funcs.jobstart(job)
@@ -36,14 +44,21 @@ class SourceKittenDaemon(object):
         else:
             raise SystemError(f"Daemon already running at job id {self.job_id}")
 
-    def _check_project_exists(self, project_name):
-        project_path = Path(project_name)
-        if project_path.exists():
-            return True
-        else:
-            return False
+    def _check_file_exists(self, file_name):
+        file_path = Path(file_name)
+        if not file_path.exists():
+            raise FileNotFoundError(f"Couldn't find file {file_name}")
+
+    def _load_vex_project(self):
+        file_name = "vex.yaml"
+        self._check_file_exists(file_name)
+        vex_proj_path = Path(file_name)
+        with vex_proj_path.open() as stream:
+            return yaml.load(stream)
+
 
     def _echom(self, msg):
+        print(msg)
         self.vim.command(f"echom \"{msg}\"")
 
 
